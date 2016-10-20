60548261c3dfdd6faf017fa5f144b2993ee00d94
Grant Buckles <grantbuckles@gmail.com>
2016-10-20 16:01:32 -0400

    Updated 2-randomization_analysis.R file; added technical appendix.

--- 2-randomization_analysis.R
+++ 2-randomization_analysis.R
@@ -1,15 +1,56 @@
 ##################
 # Geographic Randomization - Statistical Analysis of Randomization
 # Created by Grant Buckles
-# Last Updated October 11, 2016
+# Last Updated October 20, 2016
 ##################
 
 library(stringr)
 library(xtable)
 library(multcomp)
+library(timeDate)
+library(chron)
 require(descr)
-#Extract sample from randomization
+
+#Setup for .tex output
+workd <- "/Users/grantbuckles/Desktop/RSA_geographic_analysis"
+dir.create(paste0("tex_", Sys.time()))
+texFile <- tail(list.files(path=workd, pattern ="tex_2016.*"),1)[1]
+sink(file = paste0(texFile, "/analysis_dump.txt"), append = FALSE, type = c("output", "message"),
+     split = TRUE)
+invisible(file.copy(from="output_template", to=paste0(texFile,"/output_template", Sys.Date(),".tex")))
+
+#cat("Text")
+#print(xtable(table1,caption = 'Caption'),  
+#      caption.placement = 'top')
+
+#------------------------------------------------------------------------------
+# Prepare data
+#------------------------------------------------------------------------------
+
+## Extract randomized sample----------------------------------------------------
 dat1 <- filter(dat, address_treat != "")
+
+### Remove those created after April 17
+dat1 <- filter(dat1, as.Date(created_at) < '2014-04-17')
+
+### Remove duplicates (2nd entries associated with a number)
+dat1 <- filter(dat1, duplicated(dat1$msisdn) == FALSE)
+
+## Create measures of engagement-----------------------------------------------
+
+### Count variable (all instances of engagement)
+#### This variables uses the 'it_' prefix variables (columns 49-109), which 
+#### capture the time/date a response was entered; the variables that capture 
+#### the time/date that a question was asked are excluded.
+
+dat1$total.entries <- rowSums(dat1[c(49:56, 58, 61:63, 65, 67, 69, 71, 73:87, 89:109)] != "")
+table(dat1$total.entries)
+
+### Create dichotomous variable of engagement
+
+dat1$engaged <- NULL
+dat1$engaged[dat1$total.entries > 0] <- 1
+dat1$engaged[dat1$total.entries == 0] <- 0
 
 #------------------------------------------------------------------------------
 # Descriptive statistics
@@ -32,110 +73,14 @@ address_entry <- crosstab(dat2$engage_address, dat2$address_treat,
 address_entry
 print(xtable(address_entry))
 
-## 2. Additional forms of engagement-------------------------------------------
+## 2. Furthur engagement-------------------------------------------
 
-### Demographics - Age
-
-age <- crosstab(dat1$answerwin_question_age, dat1$address_treat,
-         dnn = c("Opinion", "Education"), prop.c = TRUE, prop.r = TRUE, 
+enga <- crosstab(dat1$engaged, dat1$address_treat,
+         dnn = c("Engaged", "Treatment Group"), prop.c = TRUE, prop.r = TRUE, 
          prop.t = FALSE, prop.chisq = FALSE, chisq = FALSE, plot = FALSE)
-age
-print(xtable(age))
+enga
+print(xtable(enga))
 
-
-### Demographics - Gender
-
-gender <- crosstab(dat1$answerwin_question_gender, dat1$address_treat,
-         dnn = c("Opinion", "Education"), prop.c = TRUE, prop.r = TRUE, 
-         prop.t = FALSE, prop.chisq = FALSE, chisq = FALSE, plot = FALSE)
-gender
-print(xtable(gender))
-
-
-### Demographics - Race
-
-race <- crosstab(dat1$answerwin_question_race, dat1$address_treat,
-                   dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                 prop.r = TRUE, prop.t = FALSE, prop.chisq = FALSE, 
-                 chisq = FALSE, plot = FALSE)
-race
-print(xtable(race))
-
-
-### VIP - Complete
-dat1$completedVIP[dat1$vip_complete == ""] <- "Did Not Complete"
-dat1$completedVIP[dat1$vip_complete != ""] <- "Completed"
-
-VIP <- crosstab(dat1$completedVIP, dat1$address_treat,
-                 dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                prop.r = FALSE, prop.t = FALSE, prop.chisq = FALSE, 
-                chisq = FALSE, plot = FALSE)
-VIP
-print(xtable(VIP))
-
-
-
-### VIP - Completed First Question (minimal engagemnt)
-
-VIP_q1 <- crosstab(dat1$vip_question_1, dat1$address_treat,
-                dnn = c("Opinion", "Education"), prop.c = TRUE, prop.r = FALSE,
-                prop.t = FALSE, prop.chisq = FALSE, chisq = FALSE, 
-                plot = FALSE)
-VIP_q1
-print(xtable(VIP_q1))
-
-
-### Whatsup? - Complete
-
-dat1$completed_whatsup[dat1$whatsup_complete == ""] <- "Did Not Complete"
-dat1$completed_whatsup[dat1$whatsup_complete != ""] <- "Completed"
-
-whatsup <- crosstab(dat1$completed_whatsup, dat1$address_treat,
-                dnn = c("Opinion", "Education"), prop.c = TRUE, prop.r = FALSE,
-                prop.t = FALSE, prop.chisq = FALSE, chisq = FALSE, 
-                plot = FALSE)
-whatsup
-print(xtable(whatsup))
-
-### Whatsup? - Completed First Question (minimal engagemnt)
-
-whatsup_q1 <- crosstab(dat1$whatsup_question_food_to_eat, dat1$address_treat,
-                    dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                    prop.r = FALSE, prop.t = FALSE, prop.chisq = FALSE, 
-                    chisq = FALSE, plot = FALSE)
-whatsup_q1
-print(xtable(whatsup_q1))
-
-### Feeling Thermometer - Round 1
-dat1$feeling_therm_rd1[dat1$pre_thermometer_round_1_reply == ""] <- "Did Not Answer"
-dat1$feeling_therm_rd1[dat1$pre_thermometer_round_1_reply != ""] <- "Answered"
-
-therm_1 <- crosstab(dat1$feeling_therm_rd1, dat1$address_treat,
-                       dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                    prop.r = FALSE, prop.t = FALSE, prop.chisq = FALSE, 
-                    chisq = FALSE, plot = FALSE)
-therm_1
-print(xtable(therm_1))
-
-### Feeling Thermometer - Round 2
-dat1$feeling_therm_rd2[dat1$pre_thermometer_round_2_reply == ""] <- "Did Not Answer"
-dat1$feeling_therm_rd2[dat1$pre_thermometer_round_2_reply != ""] <- "Answered"
-
-therm_2 <- crosstab(dat1$feeling_therm_rd2, dat1$address_treat,
-                    dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                    prop.r = FALSE, prop.t = FALSE, prop.chisq = FALSE, 
-                    chisq = FALSE, plot = FALSE)
-therm_2
-print(xtable(therm_2))
-
-### Engagement Question
-
-engaged <- crosstab(dat1$engagement_question, dat1$address_treat,
-                    dnn = c("Opinion", "Education"), prop.c = TRUE, 
-                    prop.r = FALSE, prop.t = FALSE, prop.chisq = FALSE, 
-                    chisq = FALSE, plot = FALSE)
-engaged
-print(xtable(engaged))
 
 #------------------------------------------------------------------------------
 # Analysis
@@ -144,28 +89,15 @@ print(xtable(engaged))
 ## 1. Test of Proportions------------------------------------------------------
 
 ### Example: Engagement Question 
-prop.test(table(dat1$engagement_question, dat1$address_treat)[,c(1,2)], 
+prop.test(table(dat1$engaged, dat1$address_treat)[,c(1,2)], 
           correct=FALSE)
-prop.test(table(dat1$engagement_question, dat1$address_treat)[,c(2,3)], 
+prop.test(table(dat1$engaged, dat1$address_treat)[,c(2,3)], 
           correct=FALSE)
-prop.test(table(dat1$engagement_question, dat1$address_treat)[,c(1,3)], 
-          correct=FALSE)
-
-### Example: Feeling Thermometer
-prop.test(table(dat1$feeling_therm_rd1, dat1$address_treat)[,c(1,2)], 
-          correct=FALSE)
-prop.test(table(dat1$feeling_therm_rd1, dat1$address_treat)[,c(2,3)], 
-          correct=FALSE)
-prop.test(table(dat1$feeling_therm_rd1, dat1$address_treat)[,c(1,3)], 
+prop.test(table(dat1$engaged, dat1$address_treat)[,c(1,3)], 
           correct=FALSE)
 
-### Example: Gender 
-prop.test(table(dat1$answerwin_question_gender, dat1$address_treat)[,c(1,2)], 
-          correct=FALSE)
-prop.test(table(dat1$answerwin_question_gender, dat1$address_treat)[,c(2,3)], 
-          correct=FALSE)
-prop.test(table(dat1$answerwin_question_gender, dat1$address_treat)[,c(1,3)], 
-          correct=FALSE)
+cat("The difference between Group A (no lookup) and the other two groups is statistically 
+    significant.")
 
 ## 2. Logistic Regression------------------------------------------------------
 
